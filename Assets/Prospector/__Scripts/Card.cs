e0e67252e015a85817e331fbf2d56e29a964ac76
stumpy252 <123128220+stumpy252@users.noreply.github.com>
2023-03-13 18:25:53 -0400

    added card backs

--- Assets/Prospector/__Scripts/Card.cs
+++ Assets/Prospector/__Scripts/Card.cs
@@ -41,6 +41,9 @@ public class Card : MonoBehaviour
          AddDecorators();
          AddPips();
          AddFace();
+        AddBack();
+        faceUp = startFaceUp;
+
 
      }
 
@@ -148,5 +151,26 @@ public class Card : MonoBehaviour
          _tGO.transform.localPosition = Vector3.zero;
          _tGO.name = faceName;
      }
+
+     /// <summary>
+     /// Property to show and hide the back of the card.
+     /// </summary>
+     public bool faceUp {
+        get { return (!back.activeSelf); }     
+        set { back.SetActive(!value); }
+     }
+    /// <summary>
+    /// Adds a back to the card so that renders on top of everything else
+    /// </summary>
+     private void AddBack() {
+         _tGO = Instantiate<GameObject>( Deck.SPRITE_PREFAB, transform );
+         _tSRend = _tGO.GetComponent<SpriteRenderer>();
+         _tSRend.sprite = CardSpritesSO.BACK;
+         _tGO.transform.localPosition = Vector3.zero;
+         // 2 is a higher sortingOrder than anything else
+         _tSRend.sortingOrder = 2;                                            // b
+         _tGO.name = "back";
+         back = _tGO;
+    }
 
 }
